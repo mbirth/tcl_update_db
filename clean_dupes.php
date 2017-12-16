30b5d08e34ea1e8f747e57702d44d29d79e74258
Markus Birth <mbirth@gmail.com>
2017-12-16 22:15:12 +0100

    Cleanup tool to remove duplicate XMLs.

---
+++ clean_dupes.php
@@ -0,0 +1,24 @@
+#!/usr/bin/env php
+<?php
+
+$bkup_dir = __DIR__ . '/data/';
+
+$file_list = glob($bkup_dir . '*.xml');
+
+$hashes = array();
+foreach ($file_list as $file) {
+    $filename = basename($file);
+    $file_hash = sha1_file($file);
+
+    if (isset($hashes[$file_hash])) {
+        $old_file = $hashes[$file_hash];
+        if (md5_file($file) == md5_file($bkup_dir . $old_file)) {
+            echo 'Duplicate file: ' . $filename . ' (first: ' . $old_file . ')' . PHP_EOL;
+            unlink($file);
+            continue;
+        }
+        echo 'Possible SHA1 collision?' . PHP_EOL;
+    }
+
+    $hashes[$file_hash] = $filename;
+}
